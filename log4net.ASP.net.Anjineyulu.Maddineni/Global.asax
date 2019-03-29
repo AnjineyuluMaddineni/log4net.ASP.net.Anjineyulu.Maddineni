484e788e029b1e8114ea268bf6b911183b2d0f69
Azure <Azure@Anji>
2019-03-29 12:09:38 +0000

    Add project files.

---
+++ log4net.ASP.net.Anjineyulu.Maddineni/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="log4net.ASP.net.Anjineyulu.Maddineni.Global" Language="C#" %>
---
+++ log4net.ASP.net.Anjineyulu.Maddineni/Global.asax.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace log4net.ASP.net.Anjineyulu.Maddineni
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+        }
+    }
+}
